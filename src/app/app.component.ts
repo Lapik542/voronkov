4b03c1edea5f605f799ee2fd7ea46c1097697f4f
Alex Gavrish <132341505+Lapik542@users.noreply.github.com>
2025-01-09 23:20:16 +0200

    add texture web-site and fix 09.01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,47 @@
 import { isPlatformBrowser } from '@angular/common'
 import {
   Component,
+  ElementRef,
   HostListener,
   Inject,
   PLATFORM_ID,
-  Renderer2
+  Renderer2,
+  ViewChild
 } from '@angular/core'
+import { trigger, transition, style, animate } from '@angular/animations'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   standalone: false,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('800ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [animate('800ms ease-out', style({ opacity: 0 }))])
+    ])
+  ]
 })
 export class AppComponent {
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef
   isActive: boolean = false
   activeCardIndex: number | null = null
   isBrowser: boolean
   activeBlock = ''
   activeModal: boolean = false
-  selectedPrice: string = '#'
+  isModalOpen = false
+  currentImage: string = ''
+  currentImageDescription: string = ''
+  currentIndex = 0
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private http: HttpClient
   ) {
     this.isBrowser = isPlatformBrowser(this.platformId)
   }
@@ -51,6 +69,13 @@ export class AppComponent {
 
   setActiveCard(index: number) {
     this.activeCardIndex = index
+    if (this.activeCardIndex === 0) {
+      this.selectedPrice = '800'
+    } else if (this.activeCardIndex === 1) {
+      this.selectedPrice = '1200'
+    } else if (this.activeCardIndex === 2) {
+      this.selectedPrice = '1600'
+    }
   }
 
   onMouseDown() {
@@ -65,5 +90,98 @@ export class AppComponent {
     if (this.isBrowser) {
       window.scrollTo({ top: 0, behavior: 'smooth' })
     }
+  }
+
+  images = [
+    {
+      src: 'assets/images/work-illus/adobe-allus.png',
+      alt: 'Adobe Allus',
+      description: 'Adobe Allus'
+    },
+    {
+      src: 'assets/images/work-illus/adobe.png',
+      alt: 'Adobe',
+      description: 'Adobe'
+    },
+    {
+      src: 'assets/images/work-illus/api.png',
+      alt: 'API',
+      description: 'API'
+    },
+    {
+      src: 'assets/images/work-illus/lend-api.png',
+      alt: 'Lend API',
+      description: 'Lend API'
+    },
+    {
+      src: 'assets/images/work-illus/nvidia.png',
+      alt: 'NVIDIA',
+      description: 'NVIDIA'
+    }
+  ]
+
+  openModalImg(image: string, description: string): void {
+    this.currentImageDescription = description
+    this.currentImage = image
+    this.isModalOpen = true
+  }
+
+  closeModalImg(): void {
+    this.isModalOpen = false
+    this.currentImage = ''
+  }
+
+  imagesWork = [
+    {
+      main: '../assets/images/test-1.png',
+      blured: '../assets/images/test-1.png',
+      title: 'Nvidia',
+      description:
+        'Once again, “How would I draw illustrations for this blog?”. This is what I got:'
+    },
+    {
+      main: '../assets/images/test-2.png',
+      blured: '../assets/images/test-2.png',
+      title: 'Adobe',
+      description:
+        'Adobe – a powerful tool for graphic designers and digital artists.'
+    }
+  ]
+
+  currentImageWork = this.imagesWork[this.currentIndex]
+
+  changeImage(index: number): void {
+    this.currentIndex = index
+    this.currentImageWork = this.imagesWork[index]
+  }
+
+  // FORM LOGIC
+
+  email: string = ''
+  selectedPrice: string = '1200'
+  message: boolean = false
+
+  submitForm() {
+    const formData = {
+      email: this.email,
+      price: this.selectedPrice
+    }
+
+    this.http
+      .post('https://voronkov-back.onrender.com/submit-form', formData)
+      .subscribe(
+        (response) => {
+          console.log('Form submitted successfully', response)
+          this.message = true
+          setTimeout(() => {
+            this.activeModal = false
+            this.renderer.removeClass(document.body, 'active-modal')
+            this.message = false
+          }, 2000)
+        },
+        (error) => {
+          console.error('Error submitting form', error)
+        }
+      )
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { CommonModule, NgClass } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms' 
+import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  imports: [NgClass, CommonModule, BrowserModule, FormsModule],
+  imports: [
+    NgClass,
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
   exports: [],
   declarations: [AppComponent],
   providers: [],
